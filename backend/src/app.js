d7fd4f69e14b64a673a862c7cf9cfef705cb156d
Ishav28 <ishavraj028@gmail.com>
2024-01-31 20:40:01 +0530

    committed by me

--- backend/src/app.js
+++ backend/src/app.js
@@ -35,6 +35,6 @@ app.post('/', (req, res) => {
 });
 
 // Start the server
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
